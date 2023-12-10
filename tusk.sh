320bf0d3469d57d914156067bcf447a9f836fe17
GennadyP19 <gennadypozdnyakov3@gmail.com>
2023-12-10 15:41:39 +0300

    output instead of changing

--- tusk.sh
+++ tusk.sh
@@ -29,3 +29,31 @@ do
 	fi
 done
 
+marker=0
+
+for var in $@
+do
+	if [[ $marker == 0 ]] &&  [[ "$var" != -* ]] && [[ $var == *.* ]] 
+	then	
+		old_name=""
+		new_name=${var%.*}${suff}.${var#*.}
+		
+		if [[ $markerd == 0 ]] 
+		then
+			echo $var $new_name
+		fi
+		
+		if [[ $markerd == 1 ]] 
+		then
+			old_name=$var
+		fi
+		
+		if [[ $markerv == 1 ]] || [[ $markerd == 1 ]]  
+		then
+			echo $old_name $new_name
+		fi
+	elif [[ $var == -- ]]
+	then
+		marker=1
+	fi
+done
